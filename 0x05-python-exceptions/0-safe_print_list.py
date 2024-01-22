77c23deb1967fbdb5ab1b6723b04e454aff30511
kotr2003 <khawlaelhaboulikotr2003@gmail.com>
2024-01-22 08:48:20 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     i=0
     try:
-        while i<x:
+        while i is not x:
             print(my_list=[i], end='')
             i+=1
     except IndexError:
         None
-        print()
-        return i
-
+    print()
+    return i
